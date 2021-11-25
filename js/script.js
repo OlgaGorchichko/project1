9bea7507733777a751b34ed2af739c63105b2cec
Olga <olga@gorchichko.com>
2021-11-25 17:18:10 +0300

    First homework

--- js/script.js
+++ js/script.js
@@ -1,9 +1,44 @@
-"use strict";
+/* Задание на урок:
 
-const category = 'toys';
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
 
-console.log(`http://someurl.com/${category}/4`);
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
 
-const user = 'Ivan';
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
 
-alert(`Hi, ${user}`);
+Проверить, чтобы все работало без ошибок в консоли */
+
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastMovie1 = prompt('Один из последних просмотренных фильмов?', ''),
+      lastMovieScore1 = prompt('На сколько оцените его?', ''),
+      lastMovie2= prompt('Один из последних просмотренных фильмов?', ''),
+      lastMovieScore2 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[lastMovie1] = lastMovieScore1;
+personalMovieDB.movies[lastMovie2] = lastMovieScore2;
+
+console.log(personalMovieDB);
